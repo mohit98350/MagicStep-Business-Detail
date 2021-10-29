b287246e8b7045693152342b693e0186e330ff2e
Mohit <mohitmayank26@gmail.com>
2021-10-29 12:11:37 +0530

    update icons and loading images

--- src/Components/Business_detail.js
+++ src/Components/Business_detail.js
@@ -1,41 +1,63 @@
 import React from 'react';
-import { Form ,Button} from 'react-bootstrap';
-import {BsFillArrowRightCircleFill,BsCameraFill } from 'react-icons/bs';
+import { useState } from 'react';
+import { Form, InputGroup, FormControl } from 'react-bootstrap';
+import { BsCameraFill,BsHouseDoor,BsGeoAlt } from 'react-icons/bs';
+import {BiStreetView} from 'react-icons/bi'
 
 const Business_detail = () => {
+  const [imagePreview, setImagePreview] = useState("");
+  const [ProimagePreview, setProImagePreview] = useState("");
+
+  const fileHandle = e => {
+    const reader = new FileReader();           // babel javascript class
+    reader.onloadend = () => {
+      setImagePreview(reader.result);
+    }
+    reader.readAsDataURL(e.target.files[0]);
+  }
+  const ProfileHandle = e => {
+    const reader = new FileReader();           // babel javascript class
+    reader.onloadend = () => {
+      setProImagePreview(reader.result);
+    }
+    reader.readAsDataURL(e.target.files[0]);
+  }
   return (
     <div>
-      <section className="business">
+      <section className="business ">
         <div className="container">
-
           <div className="cover-img border m-auto position-relative">
-
+            <div className="imagePreview">
+              {imagePreview ? <img src={imagePreview} /> : ''}
+            </div>
             <div className="d-flex justify-content-center mt-5 mb-5">
-              <h4 varient="secondary"><b>Add Cover Image</b></h4>
-                <label className="cover-icon" for="cover-pic">
-               <BsCameraFill size={30}/>
-                </label>
-              <input className="d-none" type="file" name="file" id="cover-pic" />
+              <h4 className="text" varient="secondary"><b>Add Cover Image </b></h4>
+              <label className="cover-icon position-relative" for="cover-pic">
+                <BsCameraFill size={30} />
+              </label>
+              <input className="d-none" type="file" name="file" id="cover-pic" onChange={fileHandle} />
+
             </div>
 
+            <div className="description border m-auto bg-white extra-desk-view position-absolute">
 
-
-            <div className="description border m-auto bg-white extra-desk-view">
-
-              <div className="upload-profile-img border border-white position-absolute">
-                <h5 style={{padding:10}} varient="secondary" ><b>Add Profile Image/Logo</b></h5>
-                  <label className="pro-icon" for="profile-pic" >
-               <BsCameraFill size={20}/>
+              <div className="upload-profile-img border border-white position-absolute ">
+                <div className="imagePreview">
+                  {ProimagePreview ? <img src={ProimagePreview} /> : ''}
+                </div>
+                <h5 style={{ padding: 10 }} varient="secondary" ><b>Add Profile Image/Logo</b></h5>
+                <label className="pro-icon position-absolute" for="profile-pic" >
+                  <BsCameraFill size={20} />
                 </label>
-                <input className="d-none" type="file" name="file" id="profile-pic" />
+                <input className="d-none" type="file" name="file" id="profile-pic" onChange={ProfileHandle} />
               </div>
-              <Form>
+              <Form >
                 <Form.Group className="mb-2 mt-5" controlId="">
                   <br />
                   <Form.Text className="text-muted">
                     <h4>Business Details</h4>
                   </Form.Text>
-                   <Form.Control as="select" className="input-select"> 
+                  <Form.Control as="select" className="input-select">
                     <option>Type of business</option>
                     <option value="1">One</option>
                     <option value="2">Two</option>
@@ -52,7 +74,7 @@ const Business_detail = () => {
                   <Form.Text className="text-muted">
                     <h4>Business Address</h4>
                   </Form.Text>
-                  <Form.Control as="select"className="input-select">
+                  <Form.Control as="select" className="input-select">
                     <option>Select Country</option>
                     <option value="1">India</option>
                     <option value="2">Bangladesh</option>
@@ -76,20 +98,60 @@ const Business_detail = () => {
                   <Form.Text className="text-muted">
                     <h4>Pickup Address</h4>
                   </Form.Text>
-                  <Form.Control type="text" placeholder="House/Building Name" className="light-text" />
-                  <br />
-                  <Form.Control type="text" placeholder="Street" className="light-text" />
-                  <br />
 
-                  <Form.Control type="text" placeholder="Pincode/Zipcode" className="light-text" />
+                  <InputGroup >
+
+                    <FormControl
+                      type="text"
+                      placeholder="House/Building Name"
+                      style={{ paddingLeft: '40px' }}
+                      className="light-text"
+                    />
+                    <div className="Icon">
+                    <BsHouseDoor/>
+                     
+                    </div>
+
+                  </InputGroup>
+
+                  <br />
+                  <InputGroup >
+
+                    <FormControl
+                      type="text"
+                      placeholder="Street"
+                      style={{ paddingLeft: '40px' }}
+                      className="light-text"
+                    />
+                    <div className="Icon">
+                    <BiStreetView/>
+                    </div>
+
+                  </InputGroup>
+                  <br />
+                  <InputGroup >
+
+                    <FormControl
+                      type="text"
+                      placeholder="Pincode/Zipcode"
+                      style={{ paddingLeft: '40px' }}
+                      className="light-text"
+                    />
+                    <div className="Icon">
+                   <BsGeoAlt/>
+
+
+                    </div>
+
+                  </InputGroup>
+                  
                 </Form.Group>
-                <div className="icon float-right">
-                 
-               <h3><Button variant="success" text-white style={{ border: 0 }}><BsFillArrowRightCircleFill size={30}/> </Button></h3> 
-                </div>
+                
 
               </Form>
-            
+             < div class="icon float-right form-group">
+                  <h3><button style={{ border: 'none', background: 'transparent' }}><i class="fa fa-arrow-right bg-success text-white pl-3 pr-3 pt-3 pb-3 mt-2 rounded-circle" aria-hidden="true"></i></button></h3>
+                </div>
 
             </div>
           </div>
